77450cf4226c91c823c08369de7a7032f1e44c68
sagardere <deresagar01.com>
2018-03-14 16:26:06 +0530

    file changes

--- config/auth.js
+++ config/auth.js
@@ -3,8 +3,8 @@
 module.exports = {
 
     'googleAuth' : {
-        'clientID'      : '1200658295-og86is30s95l55kcojj4i48sfnig9pp8.apps.googleusercontent.com',
-        'clientSecret'  : 'EDdJm8mVwo_Z1yu7fUlNWgMD',
+        'clientID'      : 'Your Google clientID',
+        'clientSecret'  : 'Your Google clientSecret',
         'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 };
